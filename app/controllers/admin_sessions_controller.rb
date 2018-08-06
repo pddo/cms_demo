372d2dfd1ebf402e06ef83971292b04a00ed2925
pddo <dduyphuc@gmail.com>
2018-08-06 21:47:07 +0700

    Correct session_params follow new version of Authlogic

--- app/controllers/admin_sessions_controller.rb
+++ app/controllers/admin_sessions_controller.rb
@@ -4,25 +4,29 @@ class AdminSessionsController < CmsMainController
 
   layout "cms"
 
-  LOGOUT_MSG = "Log out successful!"
-  
   def new
     @admin_session = AdminSession.new
   end
-  
+
   def create
-    @admin_session = AdminSession.new(params[:admin_session])
+    @admin_session = AdminSession.new(session_params.to_h)
     if @admin_session.save
-      flash[:notice] = "Log in successful!"
+      flash[:notice] = I18n.t('authen.login_msg')
       redirect_to rails_admin.dashboard_path
     else
       render :action => :new
     end
   end
-  
+
   def destroy
     current_admin_session.destroy
-    flash[:notice] = "Log out successful!"
+    flash[:notice] = I18n.t('authen.logout_msg')
     redirect_to main_app.login_path
+  end
+
+  protected
+
+  def session_params
+    params.require(:admin_session).permit(:login, :password, :remember_me)
   end
 end
